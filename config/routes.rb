dda9834a204c238697d0461db5b476d1d461121f
Pavel Drankov <pdrankov@corp.finam.ru>
2019-05-30 18:49:52 +0300

    add prefix: '/some-crazy-path-prefix'

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
 
-  # app init
-  match '/init', to: 'init#index', via: :get
-  match '/app',  to: 'init#index', via: :get
+  scope "/some-crazy-path-prefix" do
+    # app init
+    match '/init', to: 'init#index', via: :get
+    match '/app', to: 'init#index', via: :get
 
-  # just remember to delete public/index.html.
-  root to: 'init#index', via: :get
+    # just remember to delete public/index.html.
+    root to: 'init#index', via: :get
 
-  # load routes from external files
-  dir = File.expand_path(__dir__)
-  files = Dir.glob( "#{dir}/routes/*.rb" )
-  files.each do |file|
-    if Rails.configuration.cache_classes
-      require_dependency file
-    else
-      load file
+    # load routes from external files
+    dir = File.expand_path(__dir__)
+    files = Dir.glob("#{dir}/routes/*.rb")
+    files.each do |file|
+      if Rails.configuration.cache_classes
+        require_dependency file
+      else
+        load file
+      end
     end
+
+    match '*a', to: 'errors#routing', via: %i[get post put delete]
   end
-
-  match '*a', to: 'errors#routing', via: %i[get post put delete]
-
 end
